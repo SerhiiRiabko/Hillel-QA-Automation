22f29d295737e2b95046e412210cf7773a4942cb
Serhii-Study <adyvan2008@gmail.com>
2023-05-06 17:59:35 +0300

    2d pointof the Home Work

--- lesson_2/HomeWork_2.py
+++ lesson_2/HomeWork_2.py
@@ -1,1 +1,4 @@
-test
+Eleks = ['Rudolf Emir', 'Octav Marango', 'Dominik Toreto', 'Jeccy Tailor', 'Whitny Hyston']
+Toshiba = ['Akida Yamato', 'Yoshida Takad', 'Winhei Huan', 'Triseto Ariso', 'Ritero Naruto', 'Shibao Tikoo']
+Toshiba = Toshiba + Eleks
+
