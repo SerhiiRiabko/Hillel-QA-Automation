0b04f719843460cb10654daac571686776d48bb2
Serhii-Study <adyvan2008@gmail.com>
2023-05-07 10:16:38 +0300

    5d point of the homework and fixes

--- lesson_2/home_work2_5.py
+++ lesson_2/home_work2_5.py
@@ -1,3 +1,4 @@
 places_vip = {1: "Criss", 2: "Den", 3: "Aicek", 4: "Riddik" }
 places_common = {1: "Karl", 2: "John", 3: "Petro", 4: None }
-print(places_vip, places_common)
+print(places_vip.values(), places_common.values())
+
