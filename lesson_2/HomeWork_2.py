41b8ba4079a78add9c475a5fbdb8f6c478277144
Serhii-Study <adyvan2008@gmail.com>
2023-05-08 19:17:32 +0300

    2d point of the homework

--- lesson_2/HomeWork_2.py
+++ lesson_2/HomeWork_2.py
@@ -1,4 +1,7 @@
 Eleks = ['Rudolf Emir', 'Octav Marango', 'Dominik Toreto', 'Jeccy Tailor', 'Whitny Hyston']
 Toshiba = ['Akida Yamato', 'Yoshida Takad', 'Winhei Huan', 'Triseto Ariso', 'Ritero Naruto', 'Shibao Tikoo']
-Toshiba = Toshiba + Eleks
+Toshiba = [*Toshiba, *Eleks]
+Eleks =[]
+print(Toshiba)
+print(Eleks)
 
