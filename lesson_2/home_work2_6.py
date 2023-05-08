4e8e3eafefe80854aa1b9c2e9bbed08b3ba977b3
Serhii-Study <adyvan2008@gmail.com>
2023-05-08 19:22:04 +0300

    6d point of the homework

--- lesson_2/home_work2_6.py
+++ lesson_2/home_work2_6.py
@@ -1,6 +1,4 @@
 group_of_people = ["John", "Rebecca", "Alessandro", "Melena", "John"]
-uniq_names = []
-for names in group_of_people:
-    if names not in uniq_names:
-       uniq_names.append(names)
+uniq_names = list(set(group_of_people))
+
 print(uniq_names)
