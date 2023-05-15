2183249d775aa524432765a2e50b657af7805013
Serhii-Study <adyvan2008@gmail.com>
2023-05-15 18:02:50 +0300

    change 3d

--- Lesson_4/home_work4_3.py
+++ Lesson_4/home_work4_3.py
@@ -1,5 +1,5 @@
 import re
-text_full = "The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope " \
+text = "The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope " \
        "that was launched into low Earth orbit in 1990 and remains in operation.... " \
        "It was not the first space telescope, but it is one of the largest and most versatile, " \
        "renowned both as a vital research tool and as a public relations boon for astronomy. " \
@@ -9,5 +9,5 @@ text_full = "The Hubble.Space.Telescope (often referred to as HST or Hubble) is 
        "director of the Jet Propulsion Laboratory, was established to determine how the error could have arisen. " \
        "The Allen Commission found that a reflective null corrector, a testing device used to achieve a properly " \
        "shaped non-spherical mirror, had been incorrectly assembled—one lens was out of position by 1.3 mm (0.051 in)."
-sentences = re.split("(?<=[.!?]) +", text_full)
+sentences = re.split("(?<=[.!?]) +", text)
 print(sentences)
