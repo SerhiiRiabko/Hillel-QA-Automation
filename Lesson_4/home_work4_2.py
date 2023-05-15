c46c08810cb0595865a0c390985b5d1907a30346
Serhii-Study <adyvan2008@gmail.com>
2023-05-15 16:23:07 +0300

    again 2d point 4th work

--- Lesson_4/home_work4_2.py
+++ Lesson_4/home_work4_2.py
@@ -2,9 +2,9 @@ camel_case_names = ["FirstItem", "FriendsList", "MyTuple"]
 snake_case_names = []
 for name in camel_case_names:
     snake_case_name = name[0].lower()
-    for part2 in range(1, len(name)):
-        if name[part2].isupper():
+    for part_2 in range(1, len(name)):
+        if name[part_2].isupper():
             snake_case_name += '_'
-        snake_case_name += name[part2].lower()
+        snake_case_name += name[part_2].lower()
     snake_case_names.append(snake_case_name)
 print(snake_case_names)
