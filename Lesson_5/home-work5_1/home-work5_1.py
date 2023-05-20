6db75d8576676d6f1f49f211fe1873d7d6d2a3a5
Serhii-Study <adyvan2008@gmail.com>
2023-05-20 17:00:48 +0300

    Home work 5 1st point

--- Lesson_5/home-work5_1/home-work5_1.py
+++ Lesson_5/home-work5_1/home-work5_1.py
@@ -1,4 +1,7 @@
 import random
+import os
+import pickle
+
 # my_list = [(random.randint(1, 100), random.randint(1, 100), random.randint(1, 3)) for _ in range(100)] - see this the
 # same action - Is it correct?
 my_list = []
@@ -8,3 +11,8 @@ for _ in range(100):
     my_list.append(tuple_element)
 
 print(my_list)
+folder = os.chdir('./..')
+os.mkdir('./text_files')
+with open('./text_files/array_of_tuples.txt', 'w+b') as file:
+    data_of_file = pickle.dumps(my_list)
+    file.write(data_of_file)
