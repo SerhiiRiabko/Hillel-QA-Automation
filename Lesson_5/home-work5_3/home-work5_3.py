170a5be267af711dff3cf177ec7a54905ba2f1f8
Serhii-Study <adyvan2008@gmail.com>
2023-05-20 18:17:48 +0300

    Home work 5 3d point add

--- Lesson_5/home-work5_3/home-work5_3.py
+++ Lesson_5/home-work5_3/home-work5_3.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-with open('./../text_files/text.txt') as file:
+with open('../text_files_for_3d/text.txt') as file:
     text = file.read()
     text_s = text.lower()
     text_without_symb = re.sub(r'\W', '', text_s)
@@ -13,4 +13,3 @@ for letter in text_without_symb:
         letters_count[letter] = 1
 for letter, count in letters_count.items():
     print(f'{letter} {count}')
-
