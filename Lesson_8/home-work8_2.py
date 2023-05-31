aeb31f6a2a17feb583fb471191cf7e736eead9f1
Serhii-Study <adyvan2008@gmail.com>
2023-05-31 22:30:01 +0300

    Homework for lesson 8 changes

--- Lesson_8/home-work8_2.py
+++ Lesson_8/home-work8_2.py
@@ -1,7 +1,14 @@
-def square_of_even_numbers():
-    return (x ** 2 for x in range(1000000001) if x % 2 == 0)
+def even_squares(start, end):
+    for num in range(start, end + 1):
+        if num % 2 == 0:
+            yield num ** 2
 
 
-squares = square_of_even_numbers()
-print(next(squares))
-print(next(squares))
+# For example
+start_num = 0
+end_num = 1000000000
+
+squares = even_squares(start_num, end_num)
+
+for square in squares:
+    print(square)
