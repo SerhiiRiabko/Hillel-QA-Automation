d85816d768523dd64b0caab971b8a5c02de6186f
Serhii-Study <adyvan2008@gmail.com>
2023-06-11 17:52:42 +0300

    Changes

--- Lesson_11_OOP/home-work11_1.py
+++ Lesson_11_OOP/home-work11_1.py
@@ -1,16 +1,57 @@
 class Company:
     def __init__(self, name, industry, location):
-        self.name = name
-        self.industry = industry
-        self.location = location
+        self._name = name
+        self._industry = industry
+        self._location = location
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def industry(self):
+        return self._industry
+
+    @industry.setter
+    def industry(self, value):
+        self._industry = value
+
+    @property
+    def location(self):
+        return self._location
+
+    @location.setter
+    def location(self, value):
+        self._location = value
 
     def get_company_info(self):
-        return f'Company: {self.name}, Industry: {self.industry}, Location: {self.location}'
+        return f"Company: {self.name}, Industry: {self.industry}, Location: {self.location}"
+
+    @classmethod
+    def create_company(cls, name, industry, location):
+        return cls(name, industry, location)
+
+    @staticmethod
+    def print_static_info():
+        print("This is a static method in the Company class")
+
+    @staticmethod
+    def is_valid_company_name(name):
+        return len(name) > 0
 
 
 company = Company("Apple", "Technology", "California")
-company2 = Company("AToshiba", "Technology", "Osaka")
+print(company.get_company_info())
 
-data = company.get_company_info()
-print(data)
 
+company2 = Company.create_company("Toshiba", "Technology", "Osaka")
+print(company2.get_company_info())
+
+Company.print_static_info()
+
+
+print(Company.is_valid_company_name("Apple"))
